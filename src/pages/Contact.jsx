3a391b63632fa37a57902bb4c143cf6e0f8de7e4
Aayushi-800 <aayushirajput80064@gmail.com>
2026-09-14 21:59:02 +0530

    Fix frontend import casing

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-import Footer from "../components/Common/Footer"
+import Footer from "../components/common/Footer"
 import ReviewSlider from "../components/common/ReviewSlider"
 import ContactDetails from "../components/core/ContactPage/ContactDetails"
 import ContactForm from "../components/core/ContactPage/ContactForm"
